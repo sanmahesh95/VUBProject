ee0e781b5739d2301c3f69c186f8dfce5c592471
sanmahesh95 <118716592+sanmahesh95@users.noreply.github.com>
2022-12-29 21:31:40 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+import { useState } from "react";
+import { Routes, Route,useNavigate } from "react-router-dom";
+import Login from "./Login";
+import MyPrograms from "./scenes/MyPrograms";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
+
+function App() {
+  const [theme, colorMode] = useMode();
+ 
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+          
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/MyPrograms" element={<MyPrograms />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,107 @@
+import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+
+// color design tokens export
+export const tokens = (mode) => ({
+  ...(mode === "dark"
+    ? {
+        grey: {
+          100: "#000000",
+          200: "#c2c2c2",
+          300: "#a3a3a3",
+          400: "#858585",
+          500: "#666666",
+          600: "#525252",
+          700: "#3d3d3d",
+          800: "#292929",
+          900: "#000000",
+        },
+        primary: {
+          100: "#000000",
+         
+          300: "#c2c2c2",
+          400: "#c2c2c2",
+         
+        },
+        greenAccent: {
+          100: "#dbf5ee",
+          200: "#b7ebde",
+          300: "#94e2cd",
+          400: "#70d8bd",
+          500: "#4cceac",
+          600: "#3da58a",
+          700: "#2e7c67",
+          800: "#1e5245",
+          900: "#0f2922",
+        },
+        redAccent: {
+          100: "#f8dcdb",
+          200: "#f1b9b7",
+          300: "#e99592",
+          400: "#e2726e",
+          500: "#db4f4a",
+          600: "#af3f3b",
+          700: "#832f2c",
+          800: "#58201e",
+          900: "#2c100f",
+        },
+       
+      }
+    : {
+        
+      }),
+});
+
+// mui theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "light"
+        ? {
+        }
+        : {
+            // palette values for light mode
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.primary[100],
+            },
+           
+            background: {
+              default: "#ffffff",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 20,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+     
+     
+    },
+  };
+};
+
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return [theme];
+};
